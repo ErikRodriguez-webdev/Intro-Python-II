c4c5149aec3afe3391ae3fedbf19e859127e4094
Erik Rodriguez <eric19.21.03@gmail.com>
2020-06-03 19:02:47 -0700

    Adds imports to adv.py and Adds classes to both player and room files

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player_Info
 
 # Declare all the rooms
 
@@ -38,6 +39,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player_start = {'currently': 'outside'}
 
 # Write a loop that:
 #
@@ -49,3 +51,6 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+for i in player:
+    print(i)
--- src/player.py
+++ src/player.py
@@ -1,2 +1,6 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player_Info():
+    def __init__(self, current_room):
+        self.current_room = current_room
